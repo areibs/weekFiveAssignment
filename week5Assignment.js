d6ee44aee8c42afccc44a28fdd3a76706cdf565a
Alyssa Reibel <Alyssa@Alyssas-MacBook-Pro.local>
2022-11-06 18:44:09 -0600

    actual final commit

--- week5Assignment.js
+++ week5Assignment.js
@@ -8,22 +8,22 @@ class Recipe {
     }
 }
 
-class RecipeBook {
+class Recipebook {
     constructor(name) {
         this.name = name;
         this.recipes = [];
     }
 
-    addRecipes(recipe) {
-        if (recipe instanceof Recipe) {
-            this.recipe.push(recipe);
+    addRecipes(Recipe) {
+        if (Recipe instanceof Recipe) {
+            this.Recipe.push(Recipe);
         } else {
-            throw new Error("You can only add a type of recipe.  Argument is not a type of recipe: ${recipe}");
+            throw new Error("You can only add a type of recipe.  Argument is not a type of recipe: ${Recipe}");
         }
     }
 
     describe() {
-        return 'return ${this.name} has ${this.recipes.length} recipes.';
+        return 'return ${this.name} has ${this.Recipe.length} recipes.';
     }
 }
 
@@ -46,7 +46,7 @@ class Menu {
                     this.deleteRecipeBook();
                     break;
                 case '4':
-                    this.displayRecipeBooks();
+                    this.displayRecipeBook();
                     break;
                 default:
                     selection = 0;
@@ -57,27 +57,27 @@ class Menu {
     }
     showMainMenuOptions() {
         return prompt (`
-        0) Exit
         1) Create New Recipe Book
         2) View Recipe Book
         3) Delete Recipe Book
         4) Display All Recipe Books
+        5) Exit
         `);
     }
 
     showRecipebookMenuOptions(recipeBookInfo) {
         return prompt(`
-        0) back
-        1) create recipe
-        2) delete recipe
+        1) Add a recipe
+        2) Delete a recipe
+        3) Go back
         ------------------------
-        ${recipeBookInfo}
+        ${RecipebookInfo}
         `)
     }
 
     displayRecipeBooks() {
         let recipeBookString = '';
-        for (let i = 0; i < this.recipeBooks.length; i ++) {
+        for (let i = 0; i < this.Recipebook.length; i ++) {
             recipeBookString += i + ')' + this.recipeBooks[i].name + '\n';
         }
     alert(recipeBookString);
@@ -85,20 +85,26 @@ class Menu {
 
     createRecipeBook() {
         let name = prompt('Enter name for new recipebook:');
-        this.recipeBooks.psuh(new RecipeBook(name));
+        this.Recipebook.psuh(new RecipeBook(name));
     }
 
+    deleteRecipeBook() {
+        let index = prompt("Enter the index of the recipebook you wish to delete:");
+        if (index > -1 && index < this.Recipebook.length) {
+            this.Recipebook.splice(index, 1);
+        }
+    }
     viewRecipeBooks() {
         let index = prompt('Enter the index of the recipebook you wish to view:');
-        if (index > -1 && index < this.recipeBooks.length) {
-            this.selectedRecipebook = this.recipeBooks[index];
-            let description = "Recipe Book Name" + this.selectedRecipeBook.name + '\n';
+        if (index > -1 && index < this.Recipebook.length) {
+            this.selectedRecipebook = this.Recipebook[index];
+            let description = "Recipe Book Name" + this.selectedRecipebook.name + '\n';
 
-            for (let i = 0; i < this.selectedRecipeBook.recipes.length; i ++) {
-                description += i + ') ' + this.selectedRecipeBook.recipes[i].name + ' - ' + this.selectedRecipeBook.recipes[i].position + '\n';
+            for (let i = 0; i < this.selectedRecipebook.Recipe.length; i ++) {
+                description += i + ') ' + this.selectedRecipebook.Recipe[i].name + ' - ' + this.selectedRecipebook.Recipe[i].position + '\n';
 
             }
-            let selection = this.showRecipeBookMenuOptions(description);
+            let selection = this.showRecipebookMenuOptions(description);
             switch (selection) {
                 case '1':
                     this.createRecipe();
@@ -107,26 +113,20 @@ class Menu {
                     this.deleteRecipe();
                     break;
                 
-
             }
 
         }
     }
-    deleteRecipeBook() {
-        let index = prompt("Enter the index of the recipebook you wish to delete:");
-        if (index > -1 && index < this.recipeBooks.length) {
-            this.recipeBooks.splice(index, 1);
-        }
-    }
+    
     createRecipe() {
         let nameOfRecipe = prompt('Enter name for new recipe:');
         let ingredients = prompt("Enter ingredients for new Recipe: ");
-        this.selectedRecipeBook.recipes.push(new Recipe(nameOfRecipe, ingredients));
+        this.selectedRecipebook.Recipe.push(new Recipe(nameOfRecipe, ingredients));
     }
     deleteRecipe() {
-        let index = prompt("Enter the index of the recipe you wish to delete");
-        if (index > -1 && index , this.selectedRecipeBook.recipes.length) {
-            this.selectedRecipeBook.recipes.splice(index, 1);
+        let index = prompt("Enter the index of the recipe you wish to delete:");
+        if (index > -1 && index , this.selectedRecipebook.Recipe.length) {
+            this.selectedRecipebook.Recipe.splice(index, 1);
         }
     }
 }
